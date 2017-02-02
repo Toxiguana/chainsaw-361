17c86b2f39a0637f1ad3844ed1b98b84cc14f916
Megan <Megan@10.193.200.1>
2017-02-01 20:03:23 -0800

    STUFF!!

--- 361Lab01/src/bowling.java
+++ 361Lab01/src/bowling.java
@@ -1,4 +1,32 @@
-
 public class bowling {
 
+	public int numPins;
+	public int numFrame;
+	public int numThrow;
+	public int finalScore;
+	public int[][] frames;
+	
+	public void throwBall(int score){
+		//throw 1 - strike
+		//throw 1 - other
+			//update numThrow
+			//update numPins
+		//throw 2 - spare
+		//throw 2 - other
+			//update numThrow
+			//update numPins
+			//update frames
+	}
+	
+	public boolean isStrike(){
+		return false;
+	}
+	
+	public boolean isSpare(){
+		return false;
+	}
+	
+	public int updateScore(){
+		return 0;
+	}
 }
