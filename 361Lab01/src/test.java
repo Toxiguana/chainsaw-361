87d6a259d6f00cd88e1d58af9fb767ee39d78cf4
stevenkitscha <stevenkitscha@10.20.66.222>
2017-02-01 22:04:44 -0600

    test stuff #3

--- 361Lab01/src/test.java
+++ 361Lab01/src/test.java
@@ -1,13 +1,94 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 public class test {
-
+bowling b;
+//frames[throws][frames]
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-		//hello
+	public void testoneThrow() {
+		b= new bowling();
+		b.throwBall(7);
+		b.updateScore();
+		assertSame(7,b.finalScore);
+		assertSame(7,b.frames[0][0]);
+		assertSame(null,b.frames[1][0]);
 	}
-
+	public void testTwoThrows(){
+		b= new bowling();
+		b.throwBall(5);
+		b.throwBall(2);
+		b.updateScore();
+		assertSame(7,b.finalScore);
+		assertSame(5,b.frames[0][0]);
+		assertSame(2,b.frames[1][0]);
+		assertSame(null,b.frames[0][1]);
+	}
+	public void testThreeThrows(){
+		b= new bowling();
+		b.throwBall(5);
+		b.throwBall(2);
+		b.throwBall(5);
+		b.updateScore();
+		assertSame(12,b.finalScore);
+		assertSame(5,b.frames[0][0]);
+		assertSame(2,b.frames[1][0]);
+		assertSame(2,b.frames[0][1]);
+	}
+	public void testSpareCountsNextFrameScore(){
+		b=new bowling();
+		b.throwBall(7);
+		b.throwBall(3);
+		b.throwBall(5);
+		b.throwBall(4);
+		b.updateScore();
+		assertSame(28,b.finalScore);
+		assertSame(7,b.frames[0][0]);
+		assertSame(3,b.frames[1][0]);
+		assertSame(5,b.frames[0][1]);
+		assertSame(4,b.frames[1][1]);
+	}
+	public void testStrikeMovesToNextFrame(){
+		b=new bowling();
+		b.throwBall(10);
+		assertSame(10,b.frames[0][0]);
+		assertSame(0,b.frames[1][0]);
+		assertSame(2,b.numFrame);
+	}
+	public void testStrikeCountsNextFrameScores(){
+		b=new bowling();
+		b.throwBall(10);
+		b.throwBall(3);
+		b.throwBall(5);
+		b.throwBall(6);
+		b.throwBall(1);
+		b.updateScore();
+		assertSame(40,b.finalScore);
+		b.throwBall(4);
+		b.throwBall(4);
+		b.updateScore();
+		assertSame(48,b.finalScore);
+	}
+	public void testSpareOnLastFrame(){
+		b=new bowling();
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(3);
+		b.throwBall(5);
+		b.throwBall(5);
+		
+	}
 }
