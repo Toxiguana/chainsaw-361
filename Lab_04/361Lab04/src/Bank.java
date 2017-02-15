515a08b32c07b00ef6421d70f9352f94e7e971c0
stevenkitscha <stevenkitscha@10.20.66.221>
2017-02-15 16:30:26 -0600

    hahahah im done bitchezs

--- Lab_04/361Lab04/src/Bank.java
+++ Lab_04/361Lab04/src/Bank.java
@@ -1,4 +1,50 @@
+import java.util.ArrayList;
 
 public class Bank {
+	ArrayList<Account> accounts = new ArrayList<Account>();
 
+	public void createAccount(int accountNumber, int PIN,double startBal){
+		accounts.add(new Account(accountNumber, PIN, startBal));
+	}
+	public class Account{
+		int accountNumber;
+		int PINCode;
+		double balance;
+		public Account(int _accountnumber, int _PIN, double startbal){
+			accountNumber = _accountnumber;
+			PINCode = _PIN;
+			balance = startbal;
+		}
+		public boolean validateOperation(double amount){
+			if(amount < 0){ //withdrawal
+				if(balance + amount >= 0){
+					balance += amount;
+					return true;
+				}
+				else{
+					return false;
+				}
+			}
+			else{
+				balance += amount;
+				return true;
+			}
+		}
+
+	}
+	public boolean validatePin(int accountnumber, int pin, double amount){
+		for(Account a:accounts){
+			if(a.accountNumber == accountnumber){
+				if(a.PINCode == pin){
+					if(a.validateOperation(amount)){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
