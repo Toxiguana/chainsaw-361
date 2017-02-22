422287219a53457f601ccdb0a0132571658333df
stevenkitscha <stevenkitscha@10.20.68.149>
2017-02-22 16:13:55 -0600

    printer

--- Lab_05/Lab_05/src/Printer.java
+++ Lab_05/Lab_05/src/Printer.java
@@ -1,4 +1,8 @@
-
+import java.util.Date;
 public class Printer {
-
+	 Date date = new Date();
+public void print(String transaction,double amount){
+	System.out.println(date.toString()+" transaction :"+transaction+" amount: "+amount);
+	
+}
 }
