eb94f55cb942ac38aa5a9eb672ba7392661e8d05
Ryan McKenna <ryan.f.mckenna@gmail.com>
2017-02-22 16:13:45 -0600

    simulator commit 1

--- Lab_05/Lab_05/src/Simulator.java
+++ Lab_05/Lab_05/src/Simulator.java
@@ -1,4 +1,44 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class Simulator {
+
+
+	public void load(File file){
+
+		try {
+			Scanner f = new Scanner(file);
+			while(f.hasNextLine()){
+				String s = f.nextLine();
+				if (s.contains("cr")){
+					String amount[] = s.split(" ");
+				Integer arg = Integer.parseInt(amount[1]); 
+				}
+				if (s.contains("n")){
+
+				}
+				if (s.contains("w")){
+
+				}
+				if (s.contains("cb")){
+
+				}
+				if (s.contains("ca")){
+
+				}
+
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException("file not found");
+		}
+
+
+	}
 
 }
