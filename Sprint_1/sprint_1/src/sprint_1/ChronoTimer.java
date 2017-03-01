465ce6aa2f5c531bbb0a92d310c2582705334746
Megan <Megan@10.20.2.254>
2017-02-28 21:21:59 -0800

    doc stuff

--- Sprint_1/sprint_1/src/sprint_1/ChronoTimer.java
+++ Sprint_1/sprint_1/src/sprint_1/ChronoTimer.java
@@ -1,5 +1,27 @@
 package sprint_1;
 
 public class ChronoTimer {
+//	The first release of the timing system allows one stream of racers (IND) that can start, 
+//	finish, cancel and not finish (DNF).  These are individual times (not parallel or group).
+	
+//	For each event handle the response in the Chronotimer.
+//	You should be able to detect a start, a finish (with times) and calculate the total time for a race.
 
+//	Your system should handle the basic events:
+//	POWER
+//	EXIT
+//	RESET
+//	TIME
+//	DNF
+//	CANCEL
+//	TOG
+//	TRIG 
+//	START
+//	FINISH
+	
+//	CANCEL will discard the current race and put the racer back in the queue as 
+//	next to start.  DNF says that the run for the bib number is over (they do not 
+//	return to the queue) and the end time is a DNF (Did Not Finish).
+
+//	Output activity on the paper tape.
 }
--- Sprint_1/sprint_1/src/sprint_1/Time.java
+++ Sprint_1/sprint_1/src/sprint_1/Time.java
@@ -1,5 +1,6 @@
 package sprint_1;
 
 public class Time {
-
+//	It is recommended that you create a Time class to handle complex time 
+//	management.
 }
