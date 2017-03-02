66b402c7957d135c7e7a72eb5339f3cbb04c644c
Megan <Megan@DESKTOP-9K6OK87>
2017-03-01 20:27:05 -0800

    updating things

---
+++ Sprint_1/sprint_1/src/sprint_1/ChronoTimerTests.java
@@ -0,0 +1,14 @@
+package sprint_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChronoTimerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- Sprint_1/sprint_1/src/sprint_1/Racer.java
+++ Sprint_1/sprint_1/src/sprint_1/Racer.java
@@ -5,12 +5,14 @@ public class Racer {
 	private int numRacer;
 	private double startTime;
 	private double endTime;
+	private double elapsedTime;
 	private int stateRacer; //0 - in Queue, 1 - Racing, 2 - Done Racing
 	
-	public Racer(int nR, double sT, double eT, int sR){
+	public Racer(int nR, double sT, double enT, double elT, int sR){
 		numRacer = nR;
 		startTime = sT;
-		endTime = eT;
+		endTime = enT;
+		elapsedTime = elT;
 		stateRacer = sR;
 	}
 }
