ec8c4290f15fb2bc60f89133d265fa5ade6d9e59
Megan <Megan@10.23.168.194>
2017-03-09 21:07:57 -0800

    first stuff

--- Lab_07/Lab_07/src/Employee.java
+++ Lab_07/Lab_07/src/Employee.java
@@ -1,5 +1,19 @@
-
 public class Employee {
 	//something
 	//LastName, FirstName, Phone, Department
+	private String lastName;
+	private String firstName;
+	private int phoneNum;
+	private String depart;
+	
+	public Employee(String lN, String fN, int pN, String d){
+		lastName = lN;
+		firstName = fN;
+		phoneNum = pN;
+		depart = d;
+	}
+	@Override
+	public String toString(){
+		return lastName + " " + firstName + " " + phoneNum + " " + depart;
+	}
 }
--- Lab_07/Lab_07/src/MainDirectory.java
+++ Lab_07/Lab_07/src/MainDirectory.java
@@ -1,4 +1,60 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class MainDirectory {
-//actually has methods
+	//actually has methods
+	private Gson g = new Gson();
+	private ArrayList<Employee> employees = new ArrayList<Employee>();
+	private String stuff =  "";
+	
+	public boolean add(String lN, String fN, int pN, String d){
+		Employee e = new Employee(lN, fN, pN, d);
+		employees.add(e);
+
+		return true;
+	}
+	
+	public void end(){
+		stuff = g.toJson(employees);
+//		System.out.println(stuff);
+	}
+	
+	public void print(){
+		//print things
+		ArrayList<Employee> em = (g.fromJson(stuff, new TypeToken<Collection<Employee>>(){}.getType()));
+		
+		if(em != null && !em.isEmpty()){
+			for(Employee e: em) {
+				System.out.println(e);
+			}
+			System.out.println("");
+		}
+		else{
+			System.out.println("No elements in the MainDirectory");
+		}
+		
+	}
+	
+	public void clear(){
+		employees = new ArrayList<Employee>();
+		stuff = g.toJson(employees);
+		System.out.println("YAYYYY!");
+	}
+	
+	public static void main(String[] args) {
+		MainDirectory m = new MainDirectory();
+		m.add("r", "l", 3, "S");
+		m.add("d", "l", 3, "dklfajl");
+		m.end();
+		m.print();
+		m.add("l", "r2", 4, "SM");
+		m.end();
+		m.print();
+		m.clear();
+		m.print();
+	}
+	
 }
