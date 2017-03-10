67dc143cb7dc5894cf3154e40210c390bd2abc41
stevenkitscha <stevenkitscha@10.20.2.99>
2017-03-09 22:43:32 -0600

    push

--- Lab_07/Lab_07/src/DirectoryEditor.java
+++ Lab_07/Lab_07/src/DirectoryEditor.java
@@ -1,15 +1,42 @@
 
 public class DirectoryEditor {
+	DirectoryProxy d = new DirectoryProxy();
+	boolean addloop=false;
 	public static void main(String[] args) {
 		DirectoryProxy d = new DirectoryProxy();
-		d.add("r", "l", 3, "S");
-		d.add("d", "l", 3, "dklfajl");
+		d.add("r", "l",3,"S");
+		d.add("d", "l",3,"dklfajl");
 		d.end();
 		d.print();
-		d.add("l", "r2", 4, "SM");
+		d.add("l", "r2",4,"SM");
 		d.end();
 		d.print();
 		d.clear();
 		d.print();
+		d.add("r","l",3, "S");
+	}
+	public void sendCommand(String line){
+		while(addloop){
+			if(line.contains("END")){
+				d.end();
+			}
+			else{
+				String[] s = line.split(" ");
+				String firstName=s[0];
+				String lastName=s[1];
+				String department=s[2];
+				int phoneNumber=Integer.parseInt(s[3]);
+				d.add(lastName, firstName, phoneNumber,department);
+			}
+		}
+		if(line.contains("CLR")){
+			d.clear();
+		}
+		else if(line.contains("ADD")){
+			addloop=true;
+		}
+		else if(line.contains("PRINT")){
+			d.print();
+		}
 	}
 }
