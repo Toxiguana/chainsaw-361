1a378781a14994492a2d103bd9c82b0e1ffa1a47
Ryan <Ryan@Montauk>
2017-03-16 23:24:50 -0500

    wrote export inside of Run class

--- Sprint_2/sprint_2/src/sprint_2/Run.java
+++ Sprint_2/sprint_2/src/sprint_2/Run.java
@@ -1,12 +1,17 @@
 package sprint_2;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 public class Run {
 
 	private int runNum;
-	private ArrayList<String> runLog; 
-	
+	private ArrayList<String> runLog;
+
 	public Run(int rN, ArrayList<String> rL){
 		runNum = rN;
 		runLog = rL;		
@@ -15,6 +20,16 @@ public class Run {
 	public void print(){
 		for(int i = 0; i < runLog.size(); i++){
 			System.out.println(runLog.get(i));
+		}
+	}
+	
+	public void export() throws IOException{
+		Gson g = new Gson();
+		g.toJson(runLog);
+		
+		try(FileWriter file = new FileWriter("RUN"+runNum+".txt")){
+			file.write(g.toString());
+			System.out.println("exported Run " + runNum + " to RUN"+runNum+".txt");
 		}
 	}
 }
