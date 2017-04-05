c811732b58af628ce7e7559ac0e34318d038de24
Ryan <Ryan@Montauk>
2017-04-05 16:14:46 -0500

    Sprint_3 initial push

---
+++ Sprint_3/sprint_3/src/Racer.java
@@ -0,0 +1,40 @@
+
+
+public class Racer {
+
+	private int numRacer;
+	private double startTime;
+	private double endTime;
+	private String elapsedTime;
+	private int stateRacer; //0 - in Queue, 1 - Racing, 2 - Done Racing
+	private Time t = new Time();
+	
+	public Racer(int nR, double sT, double enT, String elT, int sR){
+		numRacer = nR;
+		startTime = sT;
+		endTime = enT;
+		elapsedTime = elT;
+		stateRacer = sR;
+	}
+	public void setStart(double sT){
+		this.startTime = sT;
+	}
+	
+	public void setState(int sR){
+		this.stateRacer = sR;
+	}
+	
+	public void setEnd(double enT){
+		this.endTime = enT;
+	}
+	
+	public void setElapsed(double sT, double enT){
+		this.elapsedTime = t.computeTime(sT, enT);
+	}
+	public int getNum(){
+		return numRacer;
+	}
+	public double getStart(){
+		return startTime;
+	}
+}
---
+++ Sprint_3/sprint_3/src/Run.java
@@ -0,0 +1,39 @@
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+public class Run {
+
+	private int runNum;
+	private ArrayList<String> runLog;
+
+	public Run(int rN, ArrayList<String> rL){
+		runNum = rN;
+		runLog = rL;		
+	}
+	
+	public int getRunNum(){
+		return runNum;
+	}
+	
+	public void print(){
+		for(int i = 0; i < runLog.size(); i++){
+			System.out.println(runLog.get(i));
+		}
+	}
+	
+	public void export() throws IOException{
+		Gson g = new Gson();
+		g.toJson(runLog);
+		
+		try(FileWriter file = new FileWriter("RUN"+runNum+".txt")){
+			file.write(g.toString());
+			System.out.println("Exported Run " + runNum + " to RUN"+runNum+".txt");
+		}
+	}
+}
