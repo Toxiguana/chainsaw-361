fdaf7b9c999d92bec768c8fa13cb03d39fab3e86
Megan <Megan@10.20.7.221>
2017-05-04 22:46:14 -0700

    fix

--- Sprint_4/sprint_4/src/Simulator.java
+++ Sprint_4/sprint_4/src/Simulator.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
- 
+
 public class Simulator {
 	//Issues events for the ChronoTimer by reading in text from file, console, or GUI.
 
