8a409fb8aa2f777018dd748599ec162f5077ce2d
Megan <Megan@10.20.7.221>
2017-05-04 22:52:46 -0700

    Committed Testcases

--- Sprint_4/sprint_4/src/ChronoTimerTests1.java
+++ Sprint_4/sprint_4/src/ChronoTimerTests1.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ChronoTimerTests {
+public class ChronoTimerTests1 {
 
 	ChronoTimer t = new ChronoTimer();
 	Time t1 = new Time();
@@ -252,38 +252,5 @@ public class ChronoTimerTests {
 		assertEquals(2, t.getRunNum());
 		assertEquals(1, t.runList.get(0).getRunNum());
 	}
-	
-	@Test
-	public void triggerWorks2RacersPARIND(){
-		t.power();
-		t.setEventType("PARIND");
-		t.newRun();
 
-		t.addRacer(22);
-		t.addRacer(23);
-		t.addRacer(24);
-		
-		assertEquals(2, t.racerQueue1.size());
-		assertEquals(1, t.racerQueue2.size());
-		assertTrue(t.racerRun1.isEmpty());
-		assertTrue(t.racerFinish2.isEmpty());
-		
-		t.togChannel(1);
-		assertTrue(t.getEnabled(0, 0));
-		t.togChannel(2);
-		t.togChannel(3);
-		t.togChannel(4);
-		
-		t.trigChannel(1);
-		t.trigChannel(1);
-		t.trigChannel(3);
-		
-		assertFalse(t.trigChannel(3));
-		assertEquals(22, t.racerRun1.peek().getNum());
-		assertEquals(23, t.racerRun2.peek().getNum());
-		
-		
-		t.power();
-		assertFalse(t.isPowerOn());
-	}
 }
--- Sprint_4/sprint_4/src/ChronoTimerTests2.java
+++ Sprint_4/sprint_4/src/ChronoTimerTests2.java
@@ -8,167 +8,36 @@ public class ChronoTimerTests2 {
 	Time t1 = new Time();
 	
 	@Test
-	public void groupTest() {
+	public void triggerWorks2RacersPARIND(){
 		t.power();
-		t.setEventType("GRP");
+		t.setEventType("PARIND");
 		t.newRun();
+
+		t.addRacer(22);
+		t.addRacer(23);
+		t.addRacer(24);
 		
-		assertTrue(t.racerQueue1.isEmpty());
+		assertEquals(2, t.racerQueue1.size());
+		assertEquals(1, t.racerQueue2.size());
+		assertTrue(t.racerRun1.isEmpty());
+		assertTrue(t.racerFinish2.isEmpty());
 		
 		t.togChannel(1);
-		t.togChannel(2);
 		assertTrue(t.getEnabled(0, 0));
-		assertTrue(t.getEnabled(1, 0));
+		t.togChannel(2);
+		t.togChannel(3);
+		t.togChannel(4);
 		
 		t.trigChannel(1);
-		assertTrue(t.racerQueue1.isEmpty());
-		assertTrue(t.racerRun1.isEmpty());
-
-		t.trigChannel(2);
-		t.trigChannel(2);
-		t.trigChannel(2);
+		t.trigChannel(1);
+		t.trigChannel(3);
 		
-		assertTrue(!t.racerFinish1.isEmpty());
-		assertTrue(t.racerFinish2.isEmpty());
-
-		assertEquals(1, t.racerFinish1.peek().getNum());
+		assertFalse(t.trigChannel(3));
+		assertEquals(22, t.racerRun1.peek().getNum());
+		assertEquals(23, t.racerRun2.peek().getNum());
+		
 		
 		t.power();
 		assertFalse(t.isPowerOn());
 	}
-	
-	@Test
-	public void groupTestAddNum() {
-		t.power();
-		t.setEventType("GRP");
-		t.newRun();
-		
-		assertTrue(t.racerQueue1.isEmpty());
-		
-		t.togChannel(1);
-		t.togChannel(2);
-		assertTrue(t.getEnabled(0, 0));
-		assertTrue(t.getEnabled(1, 0));
-		
-		t.trigChannel(1);
-		assertTrue(t.racerQueue1.isEmpty());
-		assertTrue(t.racerRun1.isEmpty());
-
-		t.trigChannel(2);
-		t.trigChannel(2);
-		t.trigChannel(2);
-		
-		assertTrue(!t.racerFinish1.isEmpty());
-		assertTrue(t.racerFinish2.isEmpty());
-
-		assertEquals(1, t.racerFinish1.peek().getNum());
-		assertEquals(3, t.racerFinish1.size());
-		
-		assertTrue(t.setGroupRacerNum(15));
-		t.setGroupRacerNum(20);
-		t.setGroupRacerNum(25);
-		
-		assertFalse(t.setGroupRacerNum(2));
-		
-		assertTrue(t.racerFinish1.isEmpty());
-		assertEquals(3, t.racerFinish2.size());
-
-		
-		t.power();
-		assertFalse(t.isPowerOn());
-	}
-	
-	@Test
-	public void groupTestAddNumAfterRun() {
-		t.power();
-		t.setEventType("GRP");
-		t.newRun();
-		
-		assertTrue(t.racerQueue1.isEmpty());
-		
-		t.togChannel(1);
-		t.togChannel(2);
-		assertTrue(t.getEnabled(0, 0));
-		assertTrue(t.getEnabled(1, 0));
-		
-		t.trigChannel(1);
-		assertTrue(t.racerQueue1.isEmpty());
-		assertTrue(t.racerRun1.isEmpty());
-
-		t.trigChannel(2);
-		t.trigChannel(2);
-		t.trigChannel(2);
-		
-		assertTrue(!t.racerFinish1.isEmpty());
-		assertTrue(t.racerFinish2.isEmpty());
-
-		assertEquals(1, t.racerFinish1.peek().getNum());
-		assertEquals(3, t.racerFinish1.size());
-		
-		t.endRun();
-		
-		Run r = t.runList.get(0);
-		
-		r.setRacerNum(0, 15);
-		r.setRacerNum(0, 20);
-		assertTrue(r.setRacerNum(0, 25));
-		
-		assertFalse(r.setRacerNum(1, 30));
-		assertFalse(t.setGroupRacerNum(2)); //cant call setgroupracernum after the run is ended		
-		
-		assertTrue(t.racerFinish1.isEmpty());
-		assertEquals(0, t.racerFinish2.size());
-
-		
-		t.power();
-		assertFalse(t.isPowerOn());
-	}
-	
-	@Test
-	public void groupTestConn() {
-		t.power();
-		t.setEventType("GRP");
-		t.newRun();
-		
-		assertTrue(t.racerQueue1.isEmpty());
-		
-		t.connectSensor(1);
-		t.connectSensor(2);
-		
-		t.togChannel(1);
-		t.togChannel(2);
-		
-		assertEquals(1, t.getConnected(0, 0).getChannelNum());
-		assertEquals(2, t.getConnected(1, 0).getChannelNum());
-		
-		t.disconnectSensor(1);
-		t.disconnectSensor(2);
-		
-		assertNull(t.getConnected(0, 0));
-		assertNull(t.getConnected(1, 0));
-	}
-	
-	@Test
-	public void swapTest(){
-		t.power();
-		t.setEventType("IND");
-		t.newRun();
-				
-		t.addRacer(12);
-		t.addRacer(13);
-		
-		t.togChannel(1);
-		t.togChannel(2);
-		assertTrue(t.getEnabled(0, 0));
-		assertTrue(t.getEnabled(1, 0));
-		
-		t.trigChannel(1);
-		t.trigChannel(1);
-		
-		t.swap();
-		
-		assertEquals(13, t.racerRun1.peek().getNum());
-
-	}
-
 }
