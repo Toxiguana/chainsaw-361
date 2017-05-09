c28d17bb1fab3f7035f8a7d125a2816158f5b582
Ryan <Ryan@Montauk>
2017-05-08 23:43:03 -0500

    more server shit

--- Sprint_4/sprint_4/src/Server.java
+++ Sprint_4/sprint_4/src/Server.java
@@ -21,7 +21,7 @@ public class Server {
         server.createContext("/displayresults/css", new cssHandler()); //listeners at the ports
 
         // create a context to get the request for the POST
-        server.createContext("/sendresults", new PostHandler()); //ONLY use this one for Lab 8
+//        server.createContext("/sendresults", new PostHandler()); //ONLY use this one for Lab 8
         server.setExecutor(null); // creates a default executor
 
         // get it going
@@ -71,9 +71,9 @@ public class Server {
             for(Racer b : run)
             {
             	response += "<tr>\n<td>" + b.getNum() + "</td>"; 
-            	response += "\n<td>" + b.getFirstInitial() + "</td>";
-            	response += "\n<td>" + b.getLast() + "</td>"; 
-            	response += "\n<td>" + b.getTime() + "</td>";
+            	response += "\n<td>" + "FIRSTNAME PLACEHOLDER" + "</td>";
+            	response += "\n<td>" + "LASTNAME PLACEHOLDER" + "</td>"; 
+            	response += "\n<td>" + b.getElapsedTime() + "</td>";
             	response += "\n</tr>";
             
             }
@@ -90,75 +90,77 @@ public class Server {
         }
     }
 
-    static class PostHandler implements HttpHandler {
-        public void handle(HttpExchange transmission) throws IOException {
-        	
-            //  shared data that is used with other handlers
-            sharedResponse = "";
-
-            // set up a stream to read the body of the request
-            InputStream inputStr = transmission.getRequestBody();
-
-            // set up a stream to write out the body of the response
-            OutputStream outputStream = transmission.getResponseBody();
-
-            // string to hold the result of reading in the request
-            StringBuilder sb = new StringBuilder();
-
-            // read the characters from the request byte by byte and build up the sharedResponse
-            int nextChar = inputStr.read();
-            while (nextChar > -1) {
-                sb = sb.append((char)nextChar);
-                nextChar = inputStr.read();
-            }
-
-            // create our response String to use in other handler
-            sharedResponse = sharedResponse + sb.toString();
-
-            System.out.println(sharedResponse);
-         
-            String[] s1 = sharedResponse.split(" ");
-            String s2 = s1[0];
-            String pr2 = "";
-            
-            if(s2.equalsIgnoreCase("Add")){
-            	String s = s1[1];
-            	String[] s3 = s.split("\"");
-            	
-            	if(s3[3].equals("")){
-            		s2 = "NOT Added";
-            		pr2 = "ERROR: Please fill out the fields!";
-            	}
-            	else{
-            		r.add(s3[3], s3[7], s3[11], s3[15]);
-//	            	System.out.println(s3[3] + s3[7] + s3[11] + s3[15]);
-            		pr2 = "Added " + s3[3] + " " + s3[7] + " to Directory.";
-            	}
-            }
-            else if(s2.equalsIgnoreCase("Clear")){
-            	r.clear();
-            	pr2 = "Cleared Directory.";
-            }
-            else if(s2.equalsIgnoreCase("Print")){
-            	pr2 = r.print();
-            }
-            
-            // respond to the POST with ROGER
-            String postResponse = "ROGER JSON RECEIVED: " + s2 + "\n" + pr2 + "\n";
-            
-            
-            System.out.println("response: " + sharedResponse + "\n");
-
-            //Desktop dt = Desktop.getDesktop();
-            //dt.open(new File("raceresults.html"));
-
-            // assume that stuff works all the time
-            transmission.sendResponseHeaders(300, postResponse.length());
-
-            // write it and return it
-            outputStream.write(postResponse.getBytes());
-
-            outputStream.close();
-        }
-    }
+//POST HANDLER.  I DONT THINK WE NEED IT    
+    
+//    static class PostHandler implements HttpHandler {
+//        public void handle(HttpExchange transmission) throws IOException {
+//        	
+//            //  shared data that is used with other handlers
+//            sharedResponse = "";
+//
+//            // set up a stream to read the body of the request
+//            InputStream inputStr = transmission.getRequestBody();
+//
+//            // set up a stream to write out the body of the response
+//            OutputStream outputStream = transmission.getResponseBody();
+//
+//            // string to hold the result of reading in the request
+//            StringBuilder sb = new StringBuilder();
+//
+//            // read the characters from the request byte by byte and build up the sharedResponse
+//            int nextChar = inputStr.read();
+//            while (nextChar > -1) {
+//                sb = sb.append((char)nextChar);
+//                nextChar = inputStr.read();
+//            }
+//
+//            // create our response String to use in other handler
+//            sharedResponse = sharedResponse + sb.toString();
+//
+//            System.out.println(sharedResponse);
+//         
+//            String[] s1 = sharedResponse.split(" ");
+//            String s2 = s1[0];
+//            String pr2 = "";
+//            
+//            if(s2.equalsIgnoreCase("Add")){
+//            	String s = s1[1];
+//            	String[] s3 = s.split("\"");
+//            	
+//            	if(s3[3].equals("")){
+//            		s2 = "NOT Added";
+//            		pr2 = "ERROR: Please fill out the fields!";
+//            	}
+//            	else{
+//            		r.add(s3[3], s3[7], s3[11], s3[15]);
+////	            	System.out.println(s3[3] + s3[7] + s3[11] + s3[15]);
+//            		pr2 = "Added " + s3[3] + " " + s3[7] + " to Directory.";
+//            	}
+//            }
+//            else if(s2.equalsIgnoreCase("Clear")){
+//            	r.clear();
+//            	pr2 = "Cleared Directory.";
+//            }
+//            else if(s2.equalsIgnoreCase("Print")){
+//            	pr2 = r.print();
+//            }
+//            
+//            // respond to the POST with ROGER
+//            String postResponse = "ROGER JSON RECEIVED: " + s2 + "\n" + pr2 + "\n";
+//            
+//            
+//            System.out.println("response: " + sharedResponse + "\n");
+//
+//            //Desktop dt = Desktop.getDesktop();
+//            //dt.open(new File("raceresults.html"));
+//
+//            // assume that stuff works all the time
+//            transmission.sendResponseHeaders(300, postResponse.length());
+//
+//            // write it and return it
+//            outputStream.write(postResponse.getBytes());
+//
+//            outputStream.close();
+//        }
+//    }
 }
